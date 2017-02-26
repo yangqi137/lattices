f44429964441d499cf15bf3fbf50389b0d8724da
Yang Qi <yangqi137@me.com>
2017-02-26 18:39:04 -0500

    working on a 1d lattice.

---
+++ src/lattices/linear/lattice.hpp
@@ -0,0 +1,42 @@
+#ifndef LATTICES_LINEAR_LATTICE_HPP
+#define LATTICES_LINEAR_LATTICE_HPP
+
+namespace lattices {
+  namespace linear {
+    struct LatticeTag {};
+    template <typename S>
+    struct Lattice {
+      typedef LatticeTag Tag;
+      S l;
+    };
+    template <typename S>
+    struct Vertex {
+      typedef LatticeTag Tag;
+
+      S x;
+    }
+    template <typename S>
+    struct LatticeCat {
+      typedef LatticeTag Tag;
+      typedef S VSize;
+      typedef S Vid;
+      typedef Lattice<S> Lattice;
+      typedef Vertex<S> Vertex;
+
+      static VSize count(const Lattice& l) { return l; }
+      static Vid vid(const Vertex& v, const Lattice& l) {
+        return v.x;
+      }
+      static Vertex vertex(Vid vid, const Lattice& l) {
+        Vertex v; v.x = vid;
+        return v;
+      }
+      static bool equal(const Vertex& v1, const Vertex& v2) {
+        return v1.x == v2.x;
+      }
+      static Vid center(const Lattice&) { return 0; }
+    }
+  }
+}
+
+#endif
---
+++ src/lattices/linear/offset.hpp
@@ -0,0 +1,71 @@
+#ifndef LATTICES_LINEAR_OFFSET_HPP
+#define LATTICES_LINEAR_OFFSET_HPP
+
+#include "lattice.hpp"
+
+#include <type_traits>
+#include <cassert>
+#include <algorithm>
+
+namespace lattices {
+  namespace linear {
+    template <typename SIZE_TYPE>
+    struct OffsetCat {
+      typedef LatticeTag Tag;
+      typedef SIZE_TYPE VSize;
+      typedef LatticeCat<VSize> LatticeCat;
+      typedef typename LatticeCat::Lattice Lattice;
+      typedef typename LatticeCat::Vertex Vertex;
+      typedef typename LatticeCat::Vid Vid;
+
+      typedef typename std::make_signed<Vid>::type OffsetType;
+      static void offset_rewind(Vid& x, OffsetType dx, Vid l) {
+	      assert(dx > -((OffsetType)l));
+	      if (dx < 0)
+	        dx += l;
+	        x += dx;
+	        x %= l;
+      }
+
+      struct Offset {
+	      OffsetType dx;
+      };
+
+      static Vid dx_max(const Lattice& l) { return l.l / 2; }
+
+      static void shift(Vertex& v, const Vertex& dv, const Lattice& l) {
+        v.x += dv.x;
+        v.x %= l.l;
+      }
+
+      static void shift(Vertex& v, const Offset& dv, const Lattice& l) {
+	      offset_rewind(v.x, dv.dx, l.lx);
+      }
+
+      static void shift(Vid& vid, Vid dvid, const Lattice& l) {
+	      Vertex v = LatticeCat::vertex(vid, l);
+	      Vertex dv = LatticeCat::vertex(dvid, l);
+	      shift(v, dv, l);
+	      vid = LatticeCat::vid(v, l);
+      }
+
+      static Offset offset(const Vertex& v0, const Vertex& v1) {
+	      Offset dv = {v1.x - v0.x};
+	      return dv;
+      }
+
+      static Vid dv2id(const Offset& dv, const Lattice& l) {
+	      Vertex v = LatticeCat::vertex(0, l);
+	      shift(v, dv, l);
+	      return LatticeCat::vid(v, l);
+      }
+
+      static Offset reverse_offset(const Offset& dv, const Lattice& l) {
+	      Offset dv2 = {-dv.dx};
+	      return dv2;
+      }
+
+    };
+  }
+}
+#endif
